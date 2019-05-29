7f4908fb5dbeb1ad0b1460ed5afe7eb50e573125
Jacob Louis Chimerine <jacobchim@Jacobs-MacBook-Pro.local>
2019-05-29 18:08:35 +0100

    edit button in details page added

--- src/app/details/details.page.ts
+++ src/app/details/details.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-details',
@@ -7,7 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DetailsPage implements OnInit {
 
-  constructor() { }
+  constructor(
+    private navCtrl: NavController
+  ) { }
+
+  navToEdit() {
+    this.navCtrl.navigateForward('edit');
+  }
 
   ngOnInit() {
   }
