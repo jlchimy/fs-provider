4889c17a6af9f884635c811005172059b655c3ce
Jacob Louis Chimerine <jacobchim@Jacobs-MacBook-Pro.local>
2019-05-30 01:27:43 +0100

    more added to submit page

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,12 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss']
 })
-export class Tab2Page {
+export class Tab2Page implements OnInit {
 
-  constructor() {}
+  public days: Array<number>;
+
+  constructor(
+    private navCtrl: NavController
+  ) {
+    this.days = new Array();
+    for (let i = 0; i < 31; i++) {
+      this.days.push(i+1);
+    }
+  }
+
+  navToRentals() {
+    this.navCtrl.navigateForward('tabs');
+  }
+
+  ngOnInit() {
+  }
 
 }
