da36f237ffcd5839bd00d30b3bccdf54c157d21c
Jacob Louis Chimerine <jacobchim@Jacobs-MBP.lan>
2019-06-10 10:41:23 +0100

    property listing fixed

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -38,7 +38,7 @@ export class Tab1Page implements OnInit {
 
   ngOnInit() {
     this.httpClient
-      .get("http://localhost:5000/api/properties/" + localStorage.getItem("userId"))
+      .get("http://localhost:5000/api/properties/provider/" + localStorage.getItem("userId"))
       .subscribe(
         (response: Array<Property>) => {
           console.log(response);
